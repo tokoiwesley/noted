4fea0a568c56e6136bc8930d4acfda39fa58c529
Wesley Tokoi <tokoiwesley@gmail.com>
2021-11-01 14:47:10 +0300

    Create a database

--- app/src/main/java/com/xfactor/noted/MainActivity.kt
+++ app/src/main/java/com/xfactor/noted/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.xfactor.noted
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.room.Room
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.xfactor.noted.database.AppDatabase
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "noted-database")
+            .allowMainThreadQueries()
+            .build()
 
         // Setting ActionBar logo
         supportActionBar?.setDisplayShowHomeEnabled(true)
@@ -23,11 +30,31 @@ class MainActivity : AppCompatActivity() {
         // Setup navigation
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_delete, R.id.navigation_listcontainer, R.id.navigation_newlist))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_delete, R.id.navigation_listcontainer, R.id.navigation_newlist
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        val listDao = db.listDao()
+        val lists: List<com.xfactor.noted.database.List> = listDao.getAll()
+
+        Log.d("lists: ", lists.toString())
+
+        // Insert a list
+        /*listDao.insertAll(
+            com.xfactor.noted.database.List(
+                title = "Test List",
+                uId = lists.last().uId + 1
+            )
+        )*/
+
+        // Delete a list
+        listDao.delete(lists[0])
+
+        Log.d("lists: ", listDao.getAll().toString())
     }
 }
 
